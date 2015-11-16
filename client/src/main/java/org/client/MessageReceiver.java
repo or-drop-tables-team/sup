76695f27fb16ce84788721959556eff23822cfd0
andelijah <colindhorowitz@gmail.com>
2015-11-15 23:59:36 -0500

    Status Messages on Message Receipt
    Adds the capability to return status messages when receiving messages.  This currently supports message confirmation, but can later be used to deny messages as well.

--- client/src/main/java/org/client/MessageReceiver.java
+++ client/src/main/java/org/client/MessageReceiver.java
@@ -1,9 +1,8 @@
 package org.client;
 
 import java.io.BufferedReader;
+import java.io.PrintWriter;
 
-import javax.swing.JTextArea;
-import javax.swing.JLabel;
 import org.common.TokenPair;
 import org.common.Utils;
 
@@ -15,14 +14,12 @@ import org.common.Utils;
 public class MessageReceiver implements Runnable {
 
     private BufferedReader in;
+    private PrintWriter out;
     private volatile boolean running;
-    JTextArea chatTxtField;
-    JLabel errLabel;
 
-    MessageReceiver( BufferedReader insock, JTextArea jTextArea, JLabel jLabel ) {
+    MessageReceiver( BufferedReader insock, PrintWriter outsock ) {
         this.in = insock;
-        this.chatTxtField = jTextArea;
-        this.errLabel = jLabel;
+        this.out = outsock;
     }
 
     /**
@@ -44,28 +41,29 @@ public class MessageReceiver implements Runnable {
             if(cmdTuple.first.equals("status")) {
                 // TODO somehow handle status messages in the normal course of operations
                 // probably move the "statusOk()" method from main client app to here
-            	
-            	// Reset the error label message for every thread
-            	this.errLabel.setText("");
                 TokenPair statusTuple = Utils.tokenize(cmdTuple.rest);
                 if(!statusTuple.first.equals("000")) {
                 	System.out.println("Error: " + statusTuple.rest);
-                	this.chatTxtField.setText(this.chatTxtField.getText());
-                	
-                	// Set the text of error label if there is any error occurs
-                	this.errLabel.setText("ERR: " + statusTuple.rest);
                 }
             } else if(cmdTuple.first.equals("recv")){
                 TokenPair userChatTuple = Utils.tokenize(cmdTuple.rest);
                 System.out.println("Got chat message from " + userChatTuple.first + ": " + userChatTuple.rest);
-                this.chatTxtField.setText(this.chatTxtField.getText() + "\n" + userChatTuple.first + " -> " + userChatTuple.rest);
+                
+                //In future there will be a variety of ways to turn down the message
+                try {
+    				//Send the server a message saying message was received
+    				Utils.sendMessage(out, Utils.MSG_RECEIVED);
+                } catch (Exception e) {
+                    System.out.println("Failed to send confirmation message to new client");
+                    e.printStackTrace();
+                }
+                
+                
             } else {
-                // Ignore it.
                 System.out.println("Unknown command message: " + cmdTuple.first);
             }
         }
     }
-    
 
     /**
      * Called when this thread should stop executing, probably at the termination of the
