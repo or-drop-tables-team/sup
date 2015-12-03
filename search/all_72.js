003df46f7c91953ebd35ce34a2d88a7f7f84e04a
Documentation Builder <sajarvis@bu.edu>
2015-12-03 03:54:55 +0000

    Automated documentation build for changeset 87fd65e33708de39e3aac7d619706c7eec38c7cd.

--- search/all_72.js
+++ search/all_72.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['reader',['reader',['../classorg_1_1server_1_1_sup_server_1_1_sup_client_handler.html#af819db373dcaf72378a61a2b62f8e171',1,'org::server::SupServer::SupClientHandler']]],
   ['receivemessage',['receiveMessage',['../classorg_1_1common_1_1_utils.html#a06e70cd6565243acd649b1a43fcc64e0',1,'org::common::Utils']]],
+  ['registeruser',['registerUser',['../classorg_1_1server_1_1_sup_server.html#a89e0542ffb5ab628bac9168173d3a6a9',1,'org::server::SupServer']]],
   ['removecontact',['removeContact',['../classorg_1_1server_1_1_contacts.html#abcff9deb1e3718d20b52e241cfe626b2',1,'org::server::Contacts.removeContact()'],['../classorg_1_1server_1_1_sup_server.html#abf151640a2709a27bf3806055d57c1b4',1,'org::server::SupServer.removeContact()']]],
   ['rest',['rest',['../classorg_1_1common_1_1_token_pair.html#aef615d349d76b73677706372251681a5',1,'org::common::TokenPair']]],
   ['run',['run',['../classorg_1_1server_1_1_sup_server_1_1_sup_client_handler.html#a6feac25a54fa7973fabd5ca0d1485392',1,'org::server::SupServer::SupClientHandler.run()'],['../classorg_1_1client_1_1_message_receiver.html#a55316106b6dbc2bcda190bffa622b27e',1,'org::client::MessageReceiver.run()']]]
